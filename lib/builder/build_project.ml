68ff7f6979a4dc00d52e62cc39d3de7143864a30
Marcos Flavio <mfghost69@gmail.com>
2023-11-17 13:42:17 -0300

    Feature :get git root dir and build simple program

--- lib/builder/build_project.ml
+++ lib/builder/build_project.ml
@@ -1,9 +1,41 @@
 (** Build project module_ *)
 module BuildProject = struct
+  let find_git_project_root () =
+    let result = Unix.open_process_in "git rev-parse --show-toplevel" in
+    input_line result
+
+  let move_root (path: string) =
+    let command path =
+      try
+        Sys.command ("cd " ^ path)
+      with
+      | Failure err -> Printf.printf "failed with error: %s" err; 1
+      | _ -> 0
+    in
+    let result = command path in
+
+    if result <> 0 then
+      failwith "Failed to move to derectory\n"
+
+  let compile () =
+    let flags = "-Wall" ^ " " ^ "-Wextra" ^ " " ^ "-Werror" ^ " " in
+    let name = "idea" in
+    let compilation_command = (
+      "gcc " ^ flags ^ "src/main.c" ^ " " ^ "-o" ^ " " ^ "./bin/" ^ name
+    ) in
+    let exit_code = Unix.system compilation_command in
+
+    match exit_code with
+    | Unix.WEXITED 0 ->
+      print_endline "compilation successful !"
+    | _ ->
+      print_endline "Compilation failed..."
+
   let build_project  = function
     | [] ->
-      (* TODO: Find project root *)
+      find_git_project_root ()
+      |> move_root;
+      compile ()
       (* TODO: Build project *)
-      print_endline "Building Project !"
     | arg :: _ -> Printf.printf "Argument %s is invalid\n" arg
 end
