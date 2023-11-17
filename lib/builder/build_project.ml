0acbd9c42e4ad5f696d4d5334d83ce4515b62122
Marcos Flavio <mfghost69@gmail.com>
2023-11-17 14:04:26 -0300

    Refact :remove move to root

--- lib/builder/build_project.ml
+++ lib/builder/build_project.ml
@@ -4,26 +4,13 @@ module BuildProject = struct
     let result = Unix.open_process_in "git rev-parse --show-toplevel" in
     input_line result
 
-  let move_root (path: string) =
-    let command path =
-      try
-        Sys.command ("cd " ^ path)
-      with
-      | Failure err -> Printf.printf "failed with error: %s" err; 1
-      | _ -> 0
-    in
-    let result = command path in
-
-    if result <> 0 then
-      failwith "Failed to move to derectory\n"
-
-  let compile () =
+  let compile (path: string) =
     let cc = "gcc" ^ " " in (* TODO: Get compiler info *)
     let debug = "" in (* TODO: Get if debug true *)
     let flags = "-Wall" ^ " " ^ "-Wextra" ^ " " ^ "-Werror" ^ " " ^ debug ^ " " in
     let name = "idea" in (* TODO: Get name of the project *)
-    let src = "src/main.c" in (* TODO: Get path to src files *)
-    let output = "-o" ^ " " ^ "./bin/" in
+    let src = path ^ "/src/main.c" in (* TODO: Get path to src files *)
+    let output = "-o" ^ " " ^ path ^ "/bin/" in
     let compilation_command = (
       cc
       ^ flags
@@ -43,7 +30,6 @@ module BuildProject = struct
   let build_project  = function
     | [] ->
       find_git_project_root ()
-      |> move_root;
-      compile ()
+      |> compile
     | arg :: _ -> Printf.printf "Argument %s is invalid\n" arg
 end
