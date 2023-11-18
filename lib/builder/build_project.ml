d378905a3479957e455b3f34fec0a589cee00097
Marcos Flavio <mfghost69@gmail.com>
2023-11-18 02:32:16 -0300

    Feature :read Prefect.toml file into a string

--- lib/builder/build_project.ml
+++ lib/builder/build_project.ml
@@ -1,10 +1,17 @@
 (** Build project module_ *)
 module BuildProject = struct
+  open Toml
   let find_git_project_root () =
     let result = Unix.open_process_in "git rev-parse --show-toplevel" in
     input_line result
 
   let compile (path: string) =
+    let read_toml (path: string) (_item: string): string =
+      let toml: Parser.result = Toml.Parser.from_filename (path ^ "/Prefect.toml") in
+      match toml with
+      | `Ok value -> Toml.Printer.string_of_table value;
+      | `Error (message, _) -> failwith message
+    in
     let cc = "gcc" in (* TODO: Get compiler info *)
     let debug = "" in (* TODO: Get if debug true *)
     let flags = "-Wall" ^ " " ^ "-Wextra" ^ " " ^ "-Werror" ^ " " ^ debug in
@@ -25,6 +32,7 @@ module BuildProject = struct
 
     match exit_code with
     | Unix.WEXITED 0 ->
+      print_endline (read_toml path "item");
       print_endline "compilation successful !"
     | _ ->
       print_endline "Compilation failed..."
