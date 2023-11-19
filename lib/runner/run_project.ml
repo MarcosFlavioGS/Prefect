50a8f555e8620f5e9d7e74c063cfb89066502b09
Marcos Flavio <mfghost69@gmail.com>
2023-11-19 00:35:27 -0300

    Feature :add partial run functionallity

--- lib/runner/run_project.ml
+++ lib/runner/run_project.ml
@@ -1,6 +1,24 @@
 (** Run Project module *)
 module RunProject = struct
+  let find_git_project_root (): string =
+    let result = Unix.open_process_in "git rev-parse --show-toplevel" in
+    input_line result
+
+  let runnner (path: string): unit =
+    let run (path: string): int =
+      try
+        Sys.command (path ^ "/bin/" ^ "idea")
+      with
+      |Failure msg -> Printf.printf "Error: %s" msg; 1
+      | _ -> 0
+    in
+
+    if run path <> 0 then
+      print_endline "Failed to run project..."
+
   let run_project = function
-    | [] -> print_endline "Run Project !"
+    | [] ->
+      find_git_project_root ()
+      |> runnner
     | arg :: _ -> Printf.printf "Argument %s is invalid\n" arg
 end
