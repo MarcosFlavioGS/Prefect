b01709988d24f89c4d91f8d6921824bbf02bf997
Marcos Flavio <mfghost69@gmail.com>
2023-11-24 01:02:42 -0300

    Feature :generate a makefile

--- lib/generator/generate.ml
+++ lib/generator/generate.ml
@@ -1,12 +1,60 @@
 (** Generate Module *)
 module GenerateStruct = struct
-    module Git = Git.Git_utils.GitUtils
+  module Git = Git.Git_utils.GitUtils
+  module Tml = Ctml.Tml.TmlUtils
+  open Str
 
-    let generate_makefile (path: string) =
+  let generate_makefile (path: string) =
+    let replace (str: string) (reg: string) (sub: string): string =
+      let regex = regexp reg in
+
+      global_replace regex sub str
+    in
+
+    let get_makefile_content (path: string) =
+      let remove_at (str: string): string =
+        let regex = regexp "\\/+" in
+        if String.length str > 1 then
+          Str.replace_first regex "" str
+        else
+          str
+      in
+
+      let name = replace (Tml.read_toml path "name") "\"+" "" in
+      let cc = replace (Tml.read_toml path "compiler") "\"+" "" in
+      let flags = (replace (Tml.read_toml path "flags") "\"+" ""
+                   |> (fun x -> replace x "\\[+" "")
+                   |> (fun x -> replace x "\\]+" "")
+                   |> (fun x -> replace x "\\,+" " ")) in
+      let src = (replace (Tml.read_toml path "src") "\"+" ""
+                 |> (fun x -> replace x "\\[+" "")
+                 |> (fun x -> replace x "\\]+" "")
+                 |> (fun x -> replace x "\\ +" "")
+                 |> (fun x -> replace x "\\,+" " ")
+                 |> (fun x -> split (regexp "\\ +") x)
+                 |> List.map (fun element -> (remove_at element) ^ " \\\n\t\t")
+                 |> (fun x -> String.concat " " x))(* TODO: Remove last \ char *)
+      in
+      (
+        "NAME = " ^ name ^ "\n\n"
+        ^"CC = " ^ cc ^ "\n\n"
+        ^ "FLAGS = " ^ flags ^ "\n\n"
+        ^ "OBJ_DIR = obj/\n\n"
+        ^ "SRC =\t" ^ src ^ "\n"
+        ^ "all:\n"
+        ^ "\t$(CC) $(FLAGS) $(SRC) -o $(NAME)" ^ "\n\n"
+        ^ "clean:\n"
+        ^ "\trm -rf $(OBJ_DIR)" ^ "\n\n"
+        ^ "fclean: clean\n"
+        ^ "\trm -rf bin/$(NAME)\n\n"
+        ^ "re: fclean all"
+      )
+    in
+
     let create_mf (path: string) (file: string): unit =
       let oc = open_out (path ^ "/" ^ file) in
 
-      output_string oc ("Hello");
+      output_string oc (get_makefile_content path);
       close_out oc
     in
 
